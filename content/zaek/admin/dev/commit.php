305c5fde748b8d34991264cdab98732ac7e28968
za-ek <tz@za-ek.ru>
2017-09-11 11:59:11 +0300

    Некорректный путь к директории с фалами версий

--- content/zaek/admin/dev/commit.php
+++ content/zaek/admin/dev/commit.php
@@ -12,12 +12,12 @@ $directory_path = $root_dir . $module;
 
 $msg = str_replace("'", '"', $_POST['msg']);
 
-if ( !file_exists($directory_path . '/content/zaek/admin/general/updates/current')) {
-    mkdir($directory_path . '/content/zaek/admin/general/updates/current', 0777, true);
+if ( !file_exists($directory_path . '/content/zaek/admin/updates/current')) {
+    mkdir($directory_path . '/content/zaek/admin/updates/current', 0777, true);
 }
 
-file_put_contents($directory_path . '/content/zaek/admin/general/updates/current/' . $module . '.php', '<?php return ' . $_POST['version'] . ';');
+file_put_contents($directory_path . '/content/zaek/admin/updates/current/' . $module . '.php', '<?php return ' . $_POST['version'] . ';');
 echo $directory_path;
-exec ("cd {$directory_path} && git add 'content/zaek/admin/general/updates/current/{$module}.php' && git commit -m '{$msg}' && git tag -a '{$_POST['version']}' -m 'version {$_POST['version']}'");
+exec ("cd {$directory_path} && git add 'content/zaek/admin/updates/current/{$module}.php' && git commit -m '{$msg}' && git tag -a '{$_POST['version']}' -m 'version {$_POST['version']}'");
 
 die();
